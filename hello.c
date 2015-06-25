c9d657b9b3fb00f85eb23c17373c94e999acd7e5
cctvv00 <chenxinghf@163.com>
2015-06-25 14:30:14 +0800

    test struct

--- hello.c
+++ hello.c
@@ -1,6 +1,13 @@
 #include "stdio.h"
+struct n{
+    int i;
+    char j;
+    float m;
+};
  int main(void)
 {
-	printf("");
+        struct n cc;
+        cc.i=1;
+	printf("%d\n",cc.i);
 	return 0;
 }
